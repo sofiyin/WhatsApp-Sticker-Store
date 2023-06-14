6ed3ca176476086764971e3044719d569163b81c
sofiahes <sofiabherreras@gmail.com>
2023-06-14 16:52:28 -0500

    personas

--- flask/routes.py
+++ flask/routes.py
@@ -12,6 +12,38 @@ def route_personas():
         db.session.add(persona)
         db.session.commit()
         return 'SUCCESS'
+    
+@app.route('/personas/<personas_id>', methods=['GET', 'PUT', 'DELETE'])
+def route_personas_id(personas_id):
+    if request.method == 'GET':
+        personas = PERSONA.query.filter_by(id=personas_id).first()
+        return jsonify(personas)
+    
+    elif request.method == 'PUT':
+        persona=request.get_json()
+        new_id=persona.get('id')
+        new_correo=persona.get('correo')
+        new_username=persona.get('username')
+        new_password=persona.get('password')
+        
+        persona_pre=PERSONA.query.get(personas_id)
+        if not persona_pre:
+            return f'La persona con el ID {personas_id} no existe'
+        
+        persona_pre.id=new_id
+        persona_pre.correo=new_correo
+        persona_pre.username=new_username
+        persona_pre.password=new_password
+        db.session.commit()
+        return 'SUCCESS'
+    
+    elif request.method == 'DELETE':
+        persona=PERSONA.query.get_or_404(personas_id)
+        db.session.delete(persona)
+        db.session.commit()
+        return 'SUCCESS'
+
+
 
 if __name__ == '__main__':
     #app.run(debug=True, port=5000, host='192.168.18.14')
